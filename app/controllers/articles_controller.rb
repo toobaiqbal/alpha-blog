68ff3c47e4c7c68c095f0e66454345b143706790
toobaiqbal <tooba.iqbal986@gmail.com>
2019-09-04 17:34:15 +0500

    Complete Blog Application

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,59 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
+  def index
+    @articles = Article.paginate(page: params[:page], per_page: 5)
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:success] = "Article was updated"
+      redirect_to article_path(@article)
+    else
+      flash[:success] = "Article was not updated"
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:danger] = "Article was deleted"
+    redirect_to articles_path
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.user = current_user
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,53 @@
+class UsersController < ApplicationController
+
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 5)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the alpha blog #{@user.username}"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:danger] = "User was deleted"
+    redirect_to users_path
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
+  end
+  private
+
+  def user_params
+  params.require(:user).permit(:username, :email, :password)
+end
+
+end
